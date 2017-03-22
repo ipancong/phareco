4227bf79260791955412bc55dacc9b0af91a6298
Harichandan Pulagam <pharic.15@gmail.com>
2017-03-21 21:18:36 -0700

    Cosine Similarity of Rating Pairs

--- src/com/phareco/spark/Recommender.scala
+++ src/com/phareco/spark/Recommender.scala
@@ -26,12 +26,62 @@ object Recommender {
     return movieNames
   }
 
+  type MovieRating = (Int, Double)
+  type UserRatingPair = (Int, (MovieRating, MovieRating))
+
+  def makePairs(userRatings: UserRatingPair) = {
+    val movieRating1 = userRatings._2._1
+    val movieRating2 = userRatings._2._2
+    ((movieRating1._1, movieRating2._1), (movieRating1._2, movieRating2._2))
+  }
+
+  def filterDuplicates(userRatings: UserRatingPair): Boolean = {
+    val movieRating1 = userRatings._2._1
+    val movieRating2 = userRatings._2._2
+
+    return movieRating1._1 < movieRating2._1
+  }
+
+  type RatingPair = (Double, Double)
+  type RatingPairs = Iterable[RatingPair]
+  
+  def cosineSimilarity(ratingPairs: RatingPairs): (Double, Int) = {
+    val numPairs: Int = 0
+    var sum_xx: Double = 0.0
+    var sum_yy: Double = 0.0
+    var sum_xy: Double = 0.0
+    
+    for (pair <- ratingPairs) {
+      val ratingX = pair._1
+      val ratingY = pair._2
+      
+      sum_xx += ratingX * ratingX
+      sum_xy += ratingX * ratingY
+      sum_yy += ratingY * ratingY
+      
+    }
+    
+    val numerator: Double = sum_xy
+    val denominator = Math.sqrt(sum_xx) * Math.sqrt(sum_yy)
+    
+    var score: Double = 0.0
+    if (denominator != 0) 
+      score = numerator / denominator
+      
+    return (score, numPairs)
+  }
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.ERROR)
     val sc = new SparkContext("local[*]", "Recommender")
     val movieNames = loadMovieNames()
     val data = sc.textFile("data/ml-100k/u.data")
-
+    val ratings = data.map(l => l.split("\t"))
+      .map(l => (l(0).toInt, (l(1).toInt, l(2).toDouble)))
+    val joinedRatings = ratings.join(ratings)
+    val uniqueJoinedRatings = joinedRatings.filter(filterDuplicates)
+    val moviePairs = uniqueJoinedRatings.map(makePairs)
+    val moviePairRatings = moviePairs.groupByKey()
+    val moviePairSimilarities = moviePairRatings.mapValues(cosineSimilarity).cache()
   }
 
 }
