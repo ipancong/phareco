9238a0c3351f222e72454f4c27591829ede4f851
Harichandan Pulagam <pharic.15@gmail.com>
2017-03-26 20:05:00 -0700

    Recommendations using Cosine Similarity

--- src/com/phareco/spark/Recommender.scala
+++ src/com/phareco/spark/Recommender.scala
@@ -32,7 +32,12 @@ object Recommender {
   def makePairs(userRatings: UserRatingPair) = {
     val movieRating1 = userRatings._2._1
     val movieRating2 = userRatings._2._2
-    ((movieRating1._1, movieRating2._1), (movieRating1._2, movieRating2._2))
+
+    val movie1 = movieRating1._1
+    val rating1 = movieRating1._2
+    val movie2 = movieRating2._1
+    val rating2 = movieRating2._2
+    ((movie1, movie2), (rating1, rating2))
   }
 
   def filterDuplicates(userRatings: UserRatingPair): Boolean = {
@@ -44,36 +49,37 @@ object Recommender {
 
   type RatingPair = (Double, Double)
   type RatingPairs = Iterable[RatingPair]
-  
+
   def cosineSimilarity(ratingPairs: RatingPairs): (Double, Int) = {
-    val numPairs: Int = 0
+    var numPairs: Int = 0
     var sum_xx: Double = 0.0
     var sum_yy: Double = 0.0
     var sum_xy: Double = 0.0
-    
+
     for (pair <- ratingPairs) {
       val ratingX = pair._1
       val ratingY = pair._2
-      
+
       sum_xx += ratingX * ratingX
       sum_xy += ratingX * ratingY
       sum_yy += ratingY * ratingY
-      
+
+      numPairs += 1
     }
-    
+
     val numerator: Double = sum_xy
     val denominator = Math.sqrt(sum_xx) * Math.sqrt(sum_yy)
-    
+
     var score: Double = 0.0
-    if (denominator != 0) 
+    if (denominator != 0)
       score = numerator / denominator
-      
+
     return (score, numPairs)
   }
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.ERROR)
     val sc = new SparkContext("local[*]", "Recommender")
-    val movieNames = loadMovieNames()
+    val movieName = loadMovieNames()
     val data = sc.textFile("data/ml-100k/u.data")
     val ratings = data.map(l => l.split("\t"))
       .map(l => (l(0).toInt, (l(1).toInt, l(2).toDouble)))
@@ -82,6 +88,37 @@ object Recommender {
     val moviePairs = uniqueJoinedRatings.map(makePairs)
     val moviePairRatings = moviePairs.groupByKey()
     val moviePairSimilarities = moviePairRatings.mapValues(cosineSimilarity).cache()
+    println(moviePairSimilarities.count())
+    if (args.length > 0) {
+      val scoreThreshold = 0.97
+      val coOccurrence = 50.0
+
+      val movieID: Int = args(0).toInt
+
+      val filteredResults = moviePairSimilarities.filter(x =>
+        {
+          val pair = x._1
+          val sim = x._2
+
+          (pair._1 == movieID || pair._2 == movieID) && sim._1 > scoreThreshold && sim._2 > coOccurrence
+        })
+
+      val results = filteredResults.map(x => (x._2, x._1)).sortByKey(false).take(10)
+
+      println("Top 10 similar movies for " + movieName(movieID))
+
+      for (result <- results) {
+        val sim = result._1
+        val pair = result._2
+
+        var similarMovieID = pair._1
+        if (similarMovieID == movieID) {
+          similarMovieID = pair._2
+        }
+
+        println(movieName(similarMovieID) + "\tScore: " + sim._1 + "\tStrength: " + sim._2)
+      }
+    }
   }
 
 }
